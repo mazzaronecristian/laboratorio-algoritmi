e6e067b3565df52bc29a20a56135c08dc0d1c002
mazzaronecristian <cristian.mazzarone@stud.unifi.it>
2023-06-09 18:39:33 +0200

    update: svolti test con tanti nodi

--- insiemi disgiunti/main.py
+++ insiemi disgiunti/main.py
@@ -2,21 +2,24 @@ import disjointSetTests
 import graph
 import node
 import plot as plt
+import random as rand
 
-node_number = [10, 20, 50, 100, 200, 1000, 5000, 10000]           #* Dimensioni degli insiemi da testare
+node_number = [50, 100, 200, 1000, 5000, 10000, 50000, 100000, 200000]           #* Dimensioni degli insiemi da testare
 
 
 #* test sulle performance di unione di insiemi disgiunti
-times = disjointSetTests.test_union_set_performance( node_number )
+# times = disjointSetTests.test_union_set_performance( node_number )
 
-plt.plot_results( node_number, list( times.values() ),
-                 list( times.keys() ),
-                "Unione di insiemi disgiunti",
-                "images/union_performace.png")
+# plt.plot_results( node_number, list( times.values() ),
+#                  list( times.keys() ),
+#                 "Unione di insiemi disgiunti",
+#                 "images/union_performace.png")
 
 
 #* test sulle performance di ricerca di componenti connesse
 graphs = []
+#genera un numero casuale compreso tra 1 e 5 usando la libreria random
+
 edge_number = [2 * x for x in node_number]                        #* Numero di archi da testare
 
 for i in range( len(node_number) ):
@@ -29,4 +32,4 @@ plt.plot_results( node_number, list( times.values() ),
                 list( times.keys() ),
                 "Ricerca di componenti connesse",
                 "images/connected_components_performance.png",
-                "n of nodes (N); n of edges (M) = N * 2" )
+                "n of nodes (N)" )
